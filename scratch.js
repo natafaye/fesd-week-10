ada50f6186aca0e33787434838324b5937c4dde6
Natalie Childs <nata.faye@gmail.com>
2024-08-06 19:40:37 -0700

    little list app

---
+++ scratch.js
@@ -0,0 +1,60 @@
+
+// document.getElementById("list")
+// Makes a little bookmark to the DOM node so we can interact with it whenever
+const myList = document.querySelector("#list")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const importantData = {
+//     id: 0,
+//     text: "hi there"
+// }
+
+// importantData.text
+
+
+
+/* <div>
+        <img src="images/chair.jpg"/>
+    </div>
+    <ul>
+        <li>List item</li>
+    </ul> */
+
+// const document = {
+//     childNodes: [
+//         {
+//             type: "div"
+//         },
+//         {
+//             type: "ul",
+//             childsNodes: [
+//                 {
+//                     type: "li"
+//                 }
+//             ]
+//         }
+//     ]
+// }
