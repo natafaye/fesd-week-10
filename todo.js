94b61227e9e2720ff8ef57cf8432b19450a0d7eb
Natalie Childs <nata.faye@gmail.com>
2025-05-06 18:32:51 -0700

    todos and number guessing

---
+++ todo.js
@@ -0,0 +1,17 @@
+const addTextbox = document.getElementById("add-input")
+const taskList = document.getElementById("task-list")
+
+function addTask() {
+    console.log( addTextbox.value )
+
+    const newBabyLi = document.createElement("li") // <li></li>
+    newBabyLi.innerHTML = addTextbox.value
+
+    // We kina have to use .addEventListener()
+    newBabyLi.addEventListener("click", () => {
+        // remove this li
+        newBabyLi.remove()
+    })
+
+    taskList.appendChild(newBabyLi) // move it into the page
+}
